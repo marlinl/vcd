7a965bc4c20dd9e8fd271baa3d847d32ecf56d55
MarlinL <malinzzz@icloud.com>
2026-05-22 17:22:58 +0800

    Fix Homebrew formula repository URLs

--- Formula/vcd.rb
+++ Formula/vcd.rb
@@ -1,10 +1,10 @@
 class Vcd < Formula
   desc "Open Git projects inside local Docker containers for vibe coding workflows"
-  homepage "https://github.com/vcd/vcd"
-  url "https://github.com/vcd/vcd/releases/download/v0.1.0/vcd-aarch64-apple-darwin.tar.gz"
+  homepage "https://github.com/marlinl/vcd"
+  url "https://github.com/marlinl/vcd/releases/download/v0.1.0/vcd-aarch64-apple-darwin.tar.gz"
   sha256 "REPLACE_WITH_RELEASE_TARBALL_SHA256"
   license "Apache-2.0"
-  head "https://github.com/vcd/vcd.git", branch: "master"
+  head "https://github.com/marlinl/vcd.git", branch: "master"
 
   on_macos do
     on_intel do
